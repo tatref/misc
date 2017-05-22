7847a8c221d31106ff5bfd2b02fa8459932d6c4a
Tatref <tatref@users.noreply.github.com>
2017-05-22 23:17:54 +0200

    Optional source port

--- python/tcpkill.py
+++ python/tcpkill.py
@@ -2,7 +2,7 @@
 #
 # tcpkill using scapy
 #
-# Usage: sudo /path/to/python tcpkill <ip> <dport> <sport>
+# Usage: sudo /path/to/python tcpkill <ip> <dport> [sport]
 #
 
 from scapy.all import *
@@ -10,18 +10,24 @@ from scapy.all import *
 
 ip = sys.argv[1]
 dport = int(sys.argv[2])
-sport = int(sys.argv[3])
+
+try:
+    sport = int(sys.argv[3])
+    f = 'host ' + ip + ' and tcp and dst port ' + str(dport) + ' and src port ' + str(sport)
+except:
+    f = 'host ' + ip + ' and tcp and dst port ' + str(dport)
+
 
 
 print('Waiting for TCP packet...')
 
 # we wait for 1 packet to arrive on the interface to know the seq number
-f = 'host ' + ip + ' and tcp and dst port ' + str(dport) + ' and src port ' + str(sport)
 p = sniff(filter=f, count=1)[0]
 
 
 # grab seq number
 seq = p.getlayer('TCP').seq
+sport = p.getlayer('TCP').sport
 
 # send RST
 send(IP(dst=ip)/TCP(dport=dport,sport=sport, seq=seq, flags='R'))
